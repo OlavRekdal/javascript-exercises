a3c85930fbd884b6b8e19b5fdd938e14bc4df4af
Fiordland Penguin <olavr98@gmail.com>
2023-02-15 22:41:16 +0100

    completed test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, value) {
+    for ( var i = 0; i < arr.length; i++ ) {
+        if (arr[i] === value) {
+            index = arr[i - 1]
+            arr.splice(index, 1); 
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
