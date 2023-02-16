1ab00dc056fd06297be724c760cc43812f1b6b40
Fiordland Penguin <olavr98@gmail.com>
2023-02-16 22:55:40 +0100

    PASSED 5.1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    var numArray = []; 
+    for ( j = num1; j <= num2; j++ ) {
+        numArray.push(j); 
+    }
 
+    // console.log(numArray); 
+
+    let sum = 0;
+    for (let i = 0; i < numArray.length; i++) {
+        sum += numArray[i];
+        
+    }
+    // console.log(sum); 
+    return sum
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
