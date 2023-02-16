cbdec6bd60fe5d04b2199279053faf092b0f57b8
Fiordland Penguin <olavr98@gmail.com>
2023-02-16 22:24:37 +0100

    PASSED exercise no. 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,19 @@
 const removeFromArray = function(arr, ...values) {
     let value = [...values]
-        console.log(value)
     for ( var j = 0; j < value.length; j++ ) {
         for ( var i = 0; i < arr.length; i++ ) {
+            // console.log(arr.length)
             if (arr[i] === value[j]) {
-                index = arr[i - 1]
+                index = i
                 arr.splice(index, 1); 
+                i--; // moves the elements in the array one left so it fits in the next iteration
+                break; // breaks out of the inner loop, and start with the next ...value
             }
         }
     }
     return arr
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
